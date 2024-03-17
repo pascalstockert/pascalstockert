82c2cd85851c445d23afab75a5c798d760390c01
Pascal Stockert <pascal.stockert@live.com>
2024-03-17 21:46:45 +0100

    Adjust styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Quicksand', defaultTheme.fontFamily.sans],
+        'serif': ['"Times New Roman"', defaultTheme.fontFamily.serif],
+      },
       screens: {
         'hr': '512px',
       },

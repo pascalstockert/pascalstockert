e70c5217d22a0cbc94ee0b4fa756980c8fd25245
Pascal Stockert <pascal.stockert@live.com>
2025-04-10 23:12:33 +0200

    set up typeorm with psql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsController } from './controllers/posts/posts.controller';
+import { PostsService } from './services/posts/posts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [
+    PostsController,
+  ],
+  providers: [
+    PostsService,
+  ],
 })
 export class AppModule {}

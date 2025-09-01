83b91f1f4119702d35dddd9d62d9c22d1106121a
Pascal Stockert <p.stockert@digital-h.de>
2025-09-01 20:28:16 +0200

    base setup for ideas-page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
+import * as path from 'node:path';
+import * as fs from 'node:fs';
 
 export default defineConfig({
   build: {
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        ideas: path.resolve(__dirname, 'src/pages/ideas/index.html'),
+      },
+    },
   },
+  plugins: [
+    {
+      name: 'rewrite-ideas',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.startsWith('/ideas')) {
+            const filePath = path.resolve(__dirname, 'src/pages/ideas/index.html')
+            const html = fs.readFileSync(filePath, 'utf-8')
+            res.setHeader('Content-Type', 'text/html')
+            res.end(html)
+            return
+          }
+          next()
+        })
+      },
+    },
+  ],
 })
